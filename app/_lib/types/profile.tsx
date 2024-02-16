f04c0a5b5608596b75186b3dc2a84d854a5fde20
BlitheBacon <39441596+BlitheBacon@users.noreply.github.com>
2024-02-15 23:31:05 -0500

    Added custom types - Added custom types for mock data for front-end development - Updated tsconfig include paths for custom type file - Created profile.json file containing mock data

---
+++ app/_lib/types/profile.tsx
@@ -0,0 +1,37 @@
+export type Location = {
+  city: string;
+  state: string;
+  country: string;
+}
+
+export type Education = {
+  institution: string;
+  degree: string;
+  fieldOfStudy: string;
+  location?: Location;
+  startDate: Date;
+  endDate: Date;
+}
+
+export enum SkillsCompetency {
+  NONE = "",
+  BEGINNER = "Beginner",
+  INTERMEDIATE = "Intermediate",
+  ADVANCED = "Advanced",
+  EXPERT = "Expert"
+};
+
+export type Skills = {
+  name: string;
+  description: string;
+  competency?: SkillsCompetency;
+}
+
+export type Experience = {
+  company_name: string;
+  title: string;
+  summary?: string;
+  location?: Location;
+  startDate: Date;
+  endDate: Date;
+}
