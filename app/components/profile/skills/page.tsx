182a9ccbe6a045196778709d7ed5d5361dbb48c7
BlitheBacon <39441596+BlitheBacon@users.noreply.github.com>
2024-02-15 23:46:47 -0500

    Skills component updated - 'Props' Type extracted from skill entry as 'Skills' for profile type definitions file - Skills component made to accept dynamic data

--- app/components/profile/skills/page.tsx
+++ app/components/profile/skills/page.tsx
@@ -1,8 +1,12 @@
 'use client';
 
+import { SkillsCompetency } from "@/app/_lib/types/profile";
 import SkillsEntry from "./skill_entry/page";
+import Data from "@/app/mock/profile/data/profile.json"
 
 export default function Skills() {
+  // Temporary values
+  const {Skill01, Skill02, Skill03, Skill04, Skill05, Skill06} = Data.Skills;
   return (
     <>
       <div className="w-full max-w-lg p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
@@ -13,14 +17,20 @@ export default function Skills() {
               </a>
         </div>
         <div className="flow-root">
-              <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
-                  <SkillsEntry skill_name="C++" skill_description="Programming Language" skill_competency="Intermediate"/>
-                  <SkillsEntry skill_name="ReactJS" skill_description="Programming Language Framework" skill_competency="Beginner"/>
-                  <SkillsEntry skill_name="C#" skill_description="Programming Language" skill_competency="Intermediate"/>
-                  <SkillsEntry skill_name="PostgreSQL" skill_description="DB Query Language" skill_competency="Intermediate"/>
-                  <SkillsEntry skill_name="Problem Solving" skill_description="Soft Skill" skill_competency="Expert"/>
-                  <SkillsEntry skill_name="Linux" skill_description="Operating System" skill_competency="Expert"/>
-              </ul>
+          <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
+            <SkillsEntry name={Skill01.name} description={Skill01.description}
+              competency={Object.values(SkillsCompetency).find((sc: string) => sc === Skill01.competency)}/>
+            <SkillsEntry name={Skill02.name} description={Skill02.description}
+              competency={Object.values(SkillsCompetency).find((sc: string) => sc === Skill02.competency)}/>
+            <SkillsEntry name={Skill03.name} description={Skill03.description}
+              competency={Object.values(SkillsCompetency).find((sc: string) => sc === Skill03.competency)}/>
+            <SkillsEntry name={Skill04.name} description={Skill04.description}
+              competency={Object.values(SkillsCompetency).find((sc: string) => sc === Skill04.competency)}/>
+            <SkillsEntry name={Skill05.name} description={Skill05.description}
+              competency={Object.values(SkillsCompetency).find((sc: string) => sc === Skill05.competency)}/>
+            <SkillsEntry name={Skill06.name} description={Skill06.description}
+              competency={Object.values(SkillsCompetency).find((sc: string) => sc === Skill06.competency)}/>
+          </ul>
         </div>
       </div>
     </>
--- app/components/profile/skills/skill_entry/page.tsx
+++ app/components/profile/skills/skill_entry/page.tsx
@@ -1,23 +1,19 @@
-type Props = {
-  skill_name: string, 
-  skill_description: string, 
-  skill_competency: string
-}
+import { Skills } from "@/app/_lib/types/profile";
 
-export default function SkillsEntry(props: Props) {
+export default function SkillsEntry(props: Skills) {
   return (
     <li className="pt-1 pb-0 sm:pt-4">
       <div className="flex items-center ">
           <div className="flex-1 min-w-0 ms-1">
               <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                  {props.skill_name}
+                  {props?.name}
               </p>
               <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                {props.skill_description}
+                {props?.description}
               </p>
           </div>
           <div className="inline-flex items-center text-base pb-4 font-semibold text-gray-900 dark:text-white">
-            {props.skill_competency}
+            {props?.competency}
           </div>
       </div>
     </li>
